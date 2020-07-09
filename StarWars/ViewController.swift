a4559cd98bf11ca53bd16cdd6e025826f8bd0b5a
DenisFeier <denis.feier@takeofflabs.com>
2020-07-09 15:16:27 +0300

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Denis Feier on 09/07/2020.
+//  Copyright © 2020 Denis Feier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
