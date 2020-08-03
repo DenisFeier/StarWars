766460b1a39b86d637523c5c2fce06d3c6f1844f
DenisFeier <denis.feier@takeofflabs.com>
2020-08-03 16:01:43 +0300

    add rocket assets and new Custom Container View for Data Details

---
+++ StarWars/ContainerDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerDetailsView.swift
+//  StarWars
+//
+//  Created by Denis Feier on 30/07/2020.
+//  Copyright © 2020 Denis Feier. All rights reserved.
+//
+
+import UIKit
+
+class ContainerDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
